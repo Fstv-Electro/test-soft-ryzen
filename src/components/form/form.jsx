af9998be3552a501f602c38c7eee46e617caf1cf
Oleksandr Kutsenko <104622826+Fstv-Electro@users.noreply.github.com>
2023-12-02 13:50:39 +0200

    fix faq items and form textarea

--- src/components/form/form.jsx
+++ src/components/form/form.jsx
@@ -14,22 +14,22 @@ export const Form = () => {
         <form className={styles.formContainer} onSubmit={handleSubmit(onSubmit)}>
             <div className={styles.formItem}>
                 <label className={styles.label}>* Full name:</label>
-                <input type="text" {...register('fullname', {required: true, pattern: /^[A-Za-z]+$/i})} className={styles.input} style={{ borderColor: errors.fullname ? '#D28B8B' : '#97d28b'}}/>
+                <input type="text" {...register('fullname', {required: true, pattern: /^[A-Za-z]+$/i})} placeholder='John Rosie' className={styles.input} style={{ borderColor: errors.fullname ? '#D28B8B' : '#97d28b'}}/>
                 {errors.fullname && <span>Wrong Fullname</span>}
             </div>
             <div className={styles.formItem}>
                 <label className={styles.label}>* E-mail:</label>
-                <input type="mail" {...register('email', {required: true, pattern: /^\S+@\S+$/i})} className={styles.input} style={{ borderColor: errors.email ? '#D28B8B' : '#97d28b'}}/>
+                <input type="mail" {...register('email', {required: true, pattern: /^\S+@\S+$/i})} placeholder='johnrosie@gmail.com' className={styles.input} style={{ borderColor: errors.email ? '#D28B8B' : '#97d28b'}}/>
                 {errors.email && <span>Wrong Email</span>}
             </div>
             <div className={styles.formItem}>
                 <label className={styles.label}>* Phone:</label>
-                <input type="tel" {...register('phone', {required: true, pattern: /^[0-9]{10}$/})} className={styles.input} style={{ borderColor: errors.phone ? '#D28B8B' : '#97d28b'}}/>
+                <input type="tel" {...register('phone', {required: true, pattern: /^[0-9]{10}$/})} placeholder='380961234567' className={styles.input} style={{ borderColor: errors.phone ? '#D28B8B' : '#97d28b'}}/>
                 {errors.phone && <span>Wrong Phone</span>}
             </div>
             <div className={styles.formItem}>
                 <label className={styles.label}>Message:</label>
-                <input type="text" {...register('message')} placeholder='My message...' className={styles.input} style={{ height: '147px'}}/>
+                <textarea {...register('message')} placeholder='Your message' className={styles.input} style={{ height: '147px', resize: 'none'}}/>
             </div>
             <button type='submit' className={styles.btn}>Send <ArrowRight/></button>
         </form>
